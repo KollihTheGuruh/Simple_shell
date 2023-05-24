1d0c5a664256f8936fbb94eb8181270d22248d4e
KollihTheGuruh <mmike8449@gmail.com>
2023-05-24 13:10:18 -0700

    shell

--- error_str_func.c
+++ error_str_func.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
+
+/**
+ * _putsfd - Writes a string to a file descriptor
+ * @str: Pointer to the string to be written
+ * @fd: File descriptor to write the string to
+ *
+ * Return: Number of characters written
+ */
+int _putsfd(char *str, int fd)
+{
+	int b = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		b += _putfd(*str++, fd);
+	}
+	return (b);
+}
+
 /**
  * _eputs - Prints a string to the standard error output
  * @str: Pointer to the string to be printed
@@ -24,27 +45,6 @@ void _eputs(char *str)
 	}
 }
 
-/**
- * _eputchar - Writes a character to the standard error
- * @c: The character to be written
- *
- * Return: On success, the character written. On failure, -1.
- */
-
-int _eputchar(char c)
-{
-	static int b;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || b >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, b);
-		b = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[b++] = c;
-	return (1);
-}
 
 /**
  * _putfd - Writes a character to a file descriptor
@@ -69,23 +69,24 @@ int _putfd(char c, int fd)
 	return (1);
 }
 
-
 /**
- * _putsfd - Writes a string to a file descriptor
- * @str: Pointer to the string to be written
- * @fd: File descriptor to write the string to
+ * _eputchar - Writes a character to the standard error
+ * @c: The character to be written
  *
- * Return: Number of characters written
+ * Return: On success, the character written. On failure, -1.
  */
-int _putsfd(char *str, int fd)
-{
-	int b = 0;
 
-	if (!str)
-		return (0);
-	while (*str)
+int _eputchar(char c)
+{
+	static int b;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || b >= WRITE_BUF_SIZE)
 	{
-		b += _putfd(*str++, fd);
+		write(2, buf, b);
+		b = 0;
 	}
-	return (b);
+	if (c != BUF_FLUSH)
+		buf[b++] = c;
+	return (1);
 }
